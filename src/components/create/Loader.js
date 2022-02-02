e87e5fc5f40c1858eb63a96f464f70e6bf26184e
EstherCastell <esthercastell2014@gmail.com>
2022-02-02 14:14:04 +0100

    json

---
+++ src/components/create/Loader.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Loader.css"
+
+//loader.io
+
+const Loader = () => {
+    return(
+        <div className="lds-ellipsis">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            </div>
+    )
+}
+
+export default Loader;
--- src/components/create/create.js
+++ src/components/create/create.js
@@ -1,29 +1,37 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import CreateTable from "./CreateTable";
 import CreateForm from "./CreateForm";
 import Navbar from "../navbar";
-import '../../styles/Create.css';
+import '../../styles/components/create.scss';
+import { helpersHttp } from "../helpers/helpersHttp";
+import Loader from "./Loader";
+import Message from "./Message";
 
-const falseDb = [{
-        "id": 1,
-        "imgName": "Beach",
-        "imgUser": "Noelia",
-        "imgURL": "beach"
-
-    },
-    {
-        "id": 2,
-        "imgName": "Mountain",
-        "imgUser": "Raquel",
-        "imgURL": "mountain"
-    }
-];
 
 
 const Create = () => {
-    const [db, setDb] = useState(falseDb);
+    const [db, setDb] = useState(null);
     const [dataToEdit, setDataToEdit] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
 
+    let api = helpersHttp();
+    let url = "http://localhost:5000/fcards";
+
+    useEffect(() => {
+        setLoading(true);
+        api.get(url).then((res) => {
+            //console.log(res);
+            if(!res.err) {
+                setDb(res);
+                setError(null);
+            }else {
+                setDb(null);
+                setError(res);
+            }
+            setLoading(false);
+        });
+    },[]);
 
     const createData = (data) => {
         //console.log(data);
@@ -54,17 +62,25 @@ const Create = () => {
         
         
         <div className="ct-create">
+            <article className="ct-create">
             <CreateForm 
             createData={createData} 
             updateData={updateData} 
             dataToEdit={dataToEdit}
             setDataToEdit = {setDataToEdit} 
             />
+            {loading&& <Loader/>}
+            {error && <Message/>}
+             {db && (
              <CreateTable 
              data={db}
              setDataToEdit = {setDataToEdit} 
              deleteData={deleteData} 
              />
+             )}
+             {/* <Loader/>
+             <Message/> */}
+             </article>
         </div>
         </div>
     );
